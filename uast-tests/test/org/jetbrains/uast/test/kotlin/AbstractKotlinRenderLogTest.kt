688bc9cb8fadecb462a37fbeee85e7abad40c9db
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-11-14 11:57:41 +0300

    UAST minor: Kotlin tests fix & refactoring

--- uast-tests/test/org/jetbrains/uast/test/kotlin/AbstractKotlinRenderLogTest.kt
+++ uast-tests/test/org/jetbrains/uast/test/kotlin/AbstractKotlinRenderLogTest.kt
@@ -1,23 +1,9 @@
 package org.jetbrains.uast.test.kotlin
 
-import org.jetbrains.uast.UFile
-import org.jetbrains.uast.asRecursiveLogString
 import org.jetbrains.uast.test.common.RenderLogTestBase
-import org.jetbrains.uast.test.env.assertEqualsToFile
 import java.io.File
 
 abstract class AbstractKotlinRenderLogTest : AbstractKotlinUastTest(), RenderLogTestBase {
     override fun getTestFile(testName: String, ext: String) =
-            File(File(TEST_KOTLIN_MODEL_DIR, testName).canonicalPath.substringBeforeLast('.') + '.' + ext)
-
-    private fun getRenderFile(testName: String) = getTestFile(testName, "render.txt")
-    private fun getLogFile(testName: String) = getTestFile(testName, "log.txt")
-
-    override fun check(testName: String, file: UFile) {
-        val renderFile = getRenderFile(testName)
-        val logFile = getLogFile(testName)
-
-        assertEqualsToFile("Render string", renderFile, file.asRenderString())
-        assertEqualsToFile("Log string", logFile, file.asRecursiveLogString())
-    }
+            File(File(TEST_KOTLIN_MODEL_DIR, testName).canonicalPath + '.' + ext)
 }
