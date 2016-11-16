dc033f5c160fd63fc9422250b4b356283c32150b
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-11-16 18:52:26 +0300

    UAST: added type tests

---
+++ uast-tests/test/org/jetbrains/uast/test/kotlin/KotlinTypesTest.kt
@@ -0,0 +1,8 @@
+package org.jetbrains.uast.test.kotlin
+
+import org.junit.Test
+
+class KotlinTypesTest : AbstractKotlinTypesTest() {
+    // TODO: fixme
+    //@Test fun testLocalClass() = doTest("LocalClass")
+}
