061813aa6e0dacc19ffd381b10bd288927e9d2d6
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-11-16 21:06:24 +0300

    UAST: temporary null PsiType for local types, relevant test

--- uast-tests/test/org/jetbrains/uast/test/kotlin/KotlinTypesTest.kt
+++ uast-tests/test/org/jetbrains/uast/test/kotlin/KotlinTypesTest.kt
@@ -3,6 +3,5 @@ package org.jetbrains.uast.test.kotlin
 import org.junit.Test
 
 class KotlinTypesTest : AbstractKotlinTypesTest() {
-    // TODO: fixme
-    //@Test fun testLocalClass() = doTest("LocalClass")
+    @Test fun testLocalClass() = doTest("LocalClass")
 }
