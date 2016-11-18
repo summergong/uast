302c7bc7ed31a65f5032071419b83fbd13616a9f
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-11-18 14:35:51 +0300

    UAST values minor: while test extended

--- uast-tests/testData/java/Simple/WhileWithContinue.java
+++ uast-tests/testData/java/Simple/WhileWithContinue.java
@@ -16,4 +16,18 @@ public class WhileWithContinue {
 
         return second;
     }
+
+    public static int baz() {
+        int first = 2;
+        int second = 2;
+
+        while (bar()) {
+            second = 3;
+            first--;
+            if (first > 0) continue;
+            second = 4;
+        }
+
+        return second;
+    }
 }
