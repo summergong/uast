70b6094a303b20ea20a5835cfbb40f3c3da988ef
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-01-12 17:57:52 +0300

    Test added for KT-15483

--- uast-tests/test/org/jetbrains/uast/test/kotlin/KotlinTypesTest.kt
+++ uast-tests/test/org/jetbrains/uast/test/kotlin/KotlinTypesTest.kt
@@ -4,4 +4,6 @@ import org.junit.Test
 
 class KotlinTypesTest : AbstractKotlinTypesTest() {
     @Test fun testLocalDeclarations() = doTest("LocalDeclarations")
+
+    @Test fun testUnexpectedContainerException() = doTest("UnexpectedContainerException")
 }
