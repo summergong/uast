2eb856fb412551e93def9a2b8a9ea4d043c0be49
Dmitry Jemerov <yole@jetbrains.com>
2017-02-06 14:45:15 +0100

    Per-file cache of UEvaluationContext

--- uast-common/src/org/jetbrains/uast/evaluation/UEvaluationContext.kt
+++ uast-common/src/org/jetbrains/uast/evaluation/UEvaluationContext.kt
@@ -1,7 +1,9 @@
 package org.jetbrains.uast.evaluation
 
+import com.intellij.openapi.util.Key
 import org.jetbrains.uast.*
 import org.jetbrains.uast.values.UValue
+import java.lang.ref.SoftReference
 
 interface UEvaluationContext {
     val uastContext: UastContext
@@ -20,7 +22,19 @@ fun UFile.analyzeAll(context: UastContext = getUastContext()): UEvaluationContex
 
 fun UExpression.uValueOf(): UValue? {
     val declaration = getContainingDeclaration() ?: return null
-    val context = MapBasedEvaluationContext(getUastContext())
+    val context = declaration.getEvaluationContextWithCaching()
     context.analyze(declaration)
     return context.valueOf(this)
 }
+
+fun UDeclaration.getEvaluationContextWithCaching(): UEvaluationContext {
+    return containingFile?.let { file ->
+        file.getUserData(EVALUATION_CONTEXT_KEY)?.get() ?:
+                MapBasedEvaluationContext(getUastContext()).apply {
+                    file.putUserData(EVALUATION_CONTEXT_KEY, SoftReference(this))
+                }
+
+    } ?: MapBasedEvaluationContext(getUastContext())
+}
+
+val EVALUATION_CONTEXT_KEY = Key<SoftReference<out UEvaluationContext>>("uast.EvaluationContext")
